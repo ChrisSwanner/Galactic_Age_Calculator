143b221e76da985cb64a0ac260b0c0d10cf3d3af
Chris Swanner <cmanswanner@gmail.com>
2018-03-16 11:22:11 -0700

    add passing test for age on mercury

--- Src/ageCalc.js
+++ Src/ageCalc.js
@@ -4,8 +4,22 @@ export class AgeCalc {
   constructor () {
 }
 
- ageInSeconds(age) {
+ageInSeconds(age) {
   let secondsYear = (age * 31536000);
   return secondsYear;
 }
+
+secondsBetweenDates(firstDate, secondDate) {
+  let date1 = moment(firstDate);
+  let date2 = moment(secondDate);
+  let resultInSeconds = date1.diff(date2, 'seconds');
+
+  return resultInSeconds;
+}
+
+ageOnMercury(age) {
+  let ageOnMercury = (Math.floor(age / .24));
+
+  return ageOnMercury;
+
 }
--- spec/calc_spec.js
+++ spec/calc_spec.js
@@ -9,3 +9,20 @@ describe("EarthYearsToSeconds", function(){
     expect(ageCalc.ageInSeconds(20)).toEqual(630720000)
   })
 })
+
+describe ("difference between two dates", function(){
+  it("should calculate the difference in seconds",function() {
+    let ageCalc = new AgeCalc();
+    let date1 = [2015, 0, 10];
+    let date2 = [2015, 0, 11];
+    expect(ageCalc.secondsBetweenDates(date1,date2)).toEqual(-86400);
+  })
+})
+
+describe ("age on mercury",function(){
+  it("should return the corret age if the user was living on mercury", function(){
+    let ageCalc = new AgeCalc();
+
+    expect(ageCalc.ageOnMercury(20)).toEqual(83);
+  })
+})
